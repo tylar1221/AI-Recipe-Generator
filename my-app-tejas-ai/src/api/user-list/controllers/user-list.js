fbcbdef04c3343541b0bfd4e9b3ccd06cb797bbf
tejas <tejassalvi47@gmail.com>
2025-03-17 22:51:54 +0530

    Initial commit with frontend and backend

---
+++ my-app-tejas-ai/src/api/user-list/controllers/user-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-list controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-list.user-list');
